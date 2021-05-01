7d1e45a870fa48ed1251bdaa7c13329e070519d5
Island <islandhuynh@gmail.com>
2021-05-01 03:12:10 -0400

    MapView is set up and user is able to search a location via search bar

--- src/screens/find-restaurant/find-restaurant-screen.tsx
+++ src/screens/find-restaurant/find-restaurant-screen.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import MapView from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import { SafeArea } from '../../components/utility/safe.area.component';
 import { Search } from './search-component';
+import { LocationContext } from '../../services/location/location-context';
 
 export const FindRestaurants = (): JSX.Element => {
+  const { location } = useContext(LocationContext);
+  const [latDelta, setLatDelta] = useState(0);
+  const { lat, lng, viewport } = location;
+
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat;
+    const southwestLat = viewport.southwest.lat;
+
+    setLatDelta(northeastLat - southwestLat);
+  }, [location, viewport]);
+
   return (
     <SafeArea>
       <Search />
+      <MapView
+        region={{
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: latDelta,
+          longitudeDelta: 0.02,
+        }}
+        style={mapStyles.map}
+      >
+      </MapView>
     </SafeArea>
   )
 }
