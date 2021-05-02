18261d93f434228ac19f6358287e5bc49a442e0b
Island <islandhuynh@gmail.com>
2021-05-01 23:46:56 -0400

    Clicking on markers will now generate a modal for the selected restaurant with further information

--- src/screens/find-restaurant/find-restaurant-screen.tsx
+++ src/screens/find-restaurant/find-restaurant-screen.tsx
@@ -1,12 +1,14 @@
 import React, { useContext, useEffect, useState } from 'react';
 import MapView, { Marker, Callout } from 'react-native-maps';
-import { StyleSheet, Dimensions, Text } from 'react-native';
+import { StyleSheet, Dimensions, Text, View, Image, Linking } from 'react-native';
 import { SafeArea } from '../../components/utility/safe.area.component';
 import { Search } from './search-component';
 import { LocationContext } from '../../services/location/location-context';
 import { RestaurantsContext } from '../../services/restaurant/restaurant-context';
 import { CompactRestaurantInfo } from './compact-restaurant-info';
 import { RestaurantInfo } from '../../categories/restaurantDetails';
+import { Button } from 'react-native-paper';
+import Modal from 'react-native-modal';
 
 export const FindRestaurants = (): JSX.Element => {
   const { location } = useContext(LocationContext);
@@ -14,6 +16,7 @@ export const FindRestaurants = (): JSX.Element => {
 
   const [latDelta, setLatDelta] = useState(0);
   const [restaurantInfoVisibility, setRestaurantInfoVisibility] = useState(false);
+  const [selectedRestaurant, setSelectedRestaurant] = useState<RestaurantInfo | null>(null);
 
   const { lat, lng, viewport } = location;
 
@@ -27,6 +30,22 @@ export const FindRestaurants = (): JSX.Element => {
   return (
     <SafeArea>
       <Search />
+      <Modal isVisible={restaurantInfoVisibility}>
+        <View style={mapStyles.restaurantInfoContainer}>
+          {selectedRestaurant ?
+            <>
+              <Text style={mapStyles.restaurantName}>{selectedRestaurant.name}</Text>
+              <Image source={{ uri: selectedRestaurant.photos[0] }} style={mapStyles.restaurantInfo} />
+              <Text>Average Rating: {selectedRestaurant.rating}</Text>
+              <Text>Price: {selectedRestaurant.price_level}</Text>
+              <Button onPress={() => Linking.openURL(`https://www.google.com/search?q=${selectedRestaurant.name}`)}>More Info</Button>
+              <Button onPress={() => setRestaurantInfoVisibility(false)}>Close</Button>
+            </>
+            :
+            null
+          }
+        </View>
+      </Modal>
       <MapView
         region={{
           latitude: lat,
@@ -46,7 +65,10 @@ export const FindRestaurants = (): JSX.Element => {
                 longitude: restaurant.geometry.location.lng
               }}
             >
-              <Callout onPress={() => setRestaurantInfoVisibility(true)}>
+              <Callout onPress={() => {
+                setRestaurantInfoVisibility(true)
+                setSelectedRestaurant(restaurant)
+              }}>
                 <CompactRestaurantInfo restaurant={restaurant} />
               </Callout>
             </Marker>
@@ -62,5 +84,31 @@ const mapStyles = StyleSheet.create({
     alignSelf: 'center',
     width: Dimensions.get('window').width - 40,
     height: Dimensions.get('window').height - 110,
+  },
+  restaurantInfo: {
+    borderRadius: 10,
+    width: 250,
+    height: 150,
+  },
+  restaurantInfoContainer: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 15,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    zIndex: 10,
+    alignItems: 'center'
+  },
+  restaurantName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    paddingBottom: 16
   }
 })
